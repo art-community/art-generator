014fd881b7c75a8bc8003c40797d497e48d4f66b
antonbashir <antonbashir@mail.ru>
2021-01-03 09:14:57 +0300

    ART Generator

--- java-example/src/main/java/ru/Example.java
+++ java-example/src/main/java/ru/Example.java
@@ -2,9 +2,13 @@ package ru;
 
 import io.art.model.annotation.*;
 import io.art.model.configurator.*;
+import org.apache.logging.log4j.*;
 import reactor.core.publisher.*;
-import static io.art.communicator.module.CommunicatorModule.communicator;
+import reactor.core.scheduler.*;
+import static io.art.communicator.module.CommunicatorModule.*;
+import static io.art.logging.LoggingModule.*;
 import static io.art.model.configurator.ModuleModelConfigurator.*;
+import java.time.*;
 
 public class Example {
     @Configurator
@@ -16,21 +20,23 @@ public class Example {
     }
 
     private static void run() {
-        communicator(MyClient.class).myMethod1();
-        communicator(MyClient.class).myMethod2(Request.builder().FBString("ART").build());
-        communicator(MyClient.class).myMethod3(Mono.just(Request.builder().FBString("ART").build()));
-        communicator(MyClient.class).myMethod4(Flux.just(Request.builder().FBString("ART").build()));
-        System.out.println("method5:" + communicator(MyClient.class).myMethod5());
-        System.out.println("method6:" +communicator(MyClient.class).myMethod6(Request.builder().FBString("ART").build()));
-        System.out.println("method7:" +communicator(MyClient.class).myMethod7(Mono.just(Request.builder().FBString("ART").build())));
-        System.out.println("method8:" +communicator(MyClient.class).myMethod8(Flux.just(Request.builder().FBString("ART").build())));
-        System.out.println("method9:" +communicator(MyClient.class).myMethod9().block());
-        System.out.println("method10:" +communicator(MyClient.class).myMethod10(Request.builder().FBString("ART").build()).block());
-        System.out.println("method11:" +communicator(MyClient.class).myMethod11(Mono.just(Request.builder().FBString("ART").build())).block());
-        System.out.println("method12:" +communicator(MyClient.class).myMethod12(Flux.just(Request.builder().FBString("ART").build())).block());
-        System.out.println("method13:" +communicator(MyClient.class).myMethod13().blockFirst());
-        System.out.println("method14:" +communicator(MyClient.class).myMethod14(Request.builder().FBString("ART").build()).blockFirst());
-        System.out.println("method15:" +communicator(MyClient.class).myMethod15(Mono.just(Request.builder().FBString("ART").build())).blockFirst());
-        System.out.println("method16:" +communicator(MyClient.class).myMethod16(Flux.just(Request.builder().FBString("ART").build())).blockFirst());
+        Logger logger = logger(Example.class);
+        MyClient myClient = communicator(MyClient.class);
+        myClient.myMethod1();
+        myClient.myMethod2(Request.builder().FBString("ART").build());
+        myClient.myMethod3(Mono.just(Request.builder().FBString("ART").build()));
+        myClient.myMethod4(Flux.just(Request.builder().FBString("ART").build()));
+        logger.info("method5:" + myClient.myMethod5());
+        logger.info("method6:" + myClient.myMethod6(Request.builder().FBString("ART").build()));
+        logger.info("method7:" + myClient.myMethod7(Mono.just(Request.builder().FBString("ART").build())));
+        logger.info("method8:" + myClient.myMethod8(Flux.just(Request.builder().FBString("ART").build())));
+        logger.info("method9:" + myClient.myMethod9().block());
+        logger.info("method10:" + myClient.myMethod10(Request.builder().FBString("ART").build()).block());
+        logger.info("method11:" + myClient.myMethod11(Mono.just(Request.builder().FBString("ART").build())).block());
+        logger.info("method12:" + myClient.myMethod12(Flux.just(Request.builder().FBString("ART").build())).block());
+        myClient.myMethod13().subscribe(data -> logger.info("method13:" + data));
+        logger.info("method14:" + myClient.myMethod14(Request.builder().FBString("ART").build()).blockFirst());
+        logger.info("method15:" + myClient.myMethod15(Mono.just(Request.builder().FBString("ART").build())).blockFirst());
+        myClient.myMethod16(Flux.interval(Duration.ofSeconds(1), Schedulers.newParallel("myMethod16-logging")).map(value -> Request.builder().FBString("ART").build())).subscribe(data -> logger.info("method16:" + data));
     }
 }
--- java-example/src/main/java/ru/MyService.java
+++ java-example/src/main/java/ru/MyService.java
@@ -2,7 +2,9 @@ package ru;
 
 import lombok.experimental.*;
 import reactor.core.publisher.*;
+import reactor.core.scheduler.*;
 import static io.art.logging.LoggingModule.*;
+import java.time.*;
 import java.util.*;
 
 @UtilityClass
@@ -12,15 +14,15 @@ public class MyService {
     }
 
     public void myMethod2(Request request) {
-        logger(MyService.class).info("myMethod2" + request);
+        logger(MyService.class).info("myMethod2:" + request);
     }
 
     public void myMethod3(Mono<Request> request) {
-        logger(MyService.class).info("myMethod3" + request.block());
+        logger(MyService.class).info("myMethod3:" + request.block());
     }
 
     public void myMethod4(Flux<Request> request) {
-        logger(MyService.class).info("myMethod4" + request.blockFirst());
+        logger(MyService.class).info("myMethod4:" + request.blockFirst());
     }
 
 
@@ -30,18 +32,18 @@ public class MyService {
     }
 
     public Response myMethod6(Request request) {
-        logger(MyService.class).info("myMethod6" + request);
+        logger(MyService.class).info("myMethod6:" + request);
         return Response.builder().build();
     }
 
     public Response myMethod7(Mono<Request> request) {
-        logger(MyService.class).info("myMethod7" + request.block());
+        logger(MyService.class).info("myMethod7:" + request.block());
         return Response.builder().build();
     }
 
     public Response myMethod8(Flux<Request> request) {
         Request first = request.blockFirst();
-        logger(MyService.class).info("myMethod8" + first);
+        logger(MyService.class).info("myMethod8:" + first);
         return Response.builder().FBString(first.getFBString()).build();
     }
 
@@ -52,39 +54,38 @@ public class MyService {
     }
 
     public Mono<Response> myMethod10(Request request) {
-        logger(MyService.class).info("myMethod10" + request);
+        logger(MyService.class).info("myMethod10:" + request);
         return Mono.just(Response.builder().build());
     }
 
     public Mono<Response> myMethod11(Mono<Request> request) {
-        logger(MyService.class).info("myMethod11" + request.block());
+        logger(MyService.class).info("myMethod11:" + request.block());
         return Mono.just(Response.builder().build());
     }
 
     public Mono<Response> myMethod12(Flux<Request> request) {
-        logger(MyService.class).info("myMethod12" + request.blockFirst());
+        logger(MyService.class).info("myMethod12:" + request.blockFirst());
         return Mono.just(Response.builder().build());
     }
 
 
     public Flux<Response> myMethod13() {
         logger(MyService.class).info("myMethod13");
-        Flux<Response> generate = Flux.generate(sink -> sink.next(Response.builder().FFloat(new Random().nextFloat()).build()));
-        return generate;
+        return Flux.interval(Duration.ofSeconds(1), Schedulers.newParallel("myMethod13")).map(index -> Response.builder().FFloat(new Random().nextFloat()).build());
     }
 
     public Flux<Response> myMethod14(Request request) {
-        logger(MyService.class).info("myMethod14" + request);
+        logger(MyService.class).info("myMethod14:" + request);
         return Flux.just(Response.builder().build());
     }
 
     public Flux<Response> myMethod15(Mono<Request> request) {
-        logger(MyService.class).info("myMethod15" + request.block());
+        logger(MyService.class).info("myMethod15:" + request.block());
         return Flux.just(Response.builder().build());
     }
 
     public Flux<Response> myMethod16(Flux<Request> request) {
-        logger(MyService.class).info("myMethod16" + request.blockFirst());
-        return Flux.just(Response.builder().build());
+        request.subscribe(data -> logger(MyService.class).info("myMethod16:" + data));
+        return Flux.interval(Duration.ofSeconds(1), Schedulers.newParallel("myMethod16")).map(index -> Response.builder().FFloat(new Random().nextFloat()).build());
     }
 }
