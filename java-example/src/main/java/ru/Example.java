a3cc3c611fa30ced0022cbb67f2cea97a4ea04fa
antonbashir <antonbashir@mail.ru>
2021-01-05 08:18:44 +0300

    ART Generator

--- java-example/src/main/java/ru/Example.java
+++ java-example/src/main/java/ru/Example.java
@@ -1,5 +1,6 @@
 package ru;
 
+import io.art.communicator.module.*;
 import io.art.model.annotation.*;
 import io.art.model.configurator.*;
 import static io.art.model.configurator.ModuleModelConfigurator.*;
@@ -8,6 +9,7 @@ public class Example {
     @Configurator
     public static ModuleModelConfigurator configure() {
         return module(Example.class)
+                .onLoad(() -> CommunicatorModule.communicator(MyClient.class).myMethod1())
                 .serve(server -> server.rsocket(MyService.class, RsocketServiceModelConfigurator::enableLogging))
                 .communicate(communicator -> communicator.rsocket(MyClient.class, client -> client.to(MyService.class)));
     }
