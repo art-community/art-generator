77f85056eb2233d1ca31c27b5d5611e3a218e455
antonbashir <antonbashir@mail.ru>
2021-01-08 09:04:09 +0300

    ART Generator

--- java-example/src/main/java/ru/Example.java
+++ java-example/src/main/java/ru/Example.java
@@ -6,9 +6,11 @@ import io.art.model.annotation.*;
 import io.art.model.configurator.*;
 import ru.communicator.*;
 import ru.configuration.*;
+import ru.model.*;
 import ru.service.*;
 import static io.art.communicator.module.CommunicatorModule.*;
 import static io.art.configurator.module.ConfiguratorModule.*;
+import static io.art.core.factory.ArrayFactory.arrayOf;
 import static io.art.core.magic.MagicRunner.*;
 import static io.art.model.configurator.ModuleModelConfigurator.*;
 import static io.art.server.constants.ServerModuleConstants.RequestValidationPolicy.*;
@@ -28,7 +30,7 @@ public class Example {
                     MyConfig config = configuration("config", MyConfig.class);
                     System.out.println(config);
                     System.out.println(configuration("config").getNested("SIM").asMap(sim -> sim.asArray(NestedConfiguration::asString)));
-                    communicator(MyClient.class).myMethod1();
+                    communicator(MyClient.class).myMethod2(Request.builder().FABString(arrayOf("test", "test")).build());
                 })
                 .onLoad(() -> System.out.println(configuration(MyConfigParent.class)))
                 .onLoad(() -> System.out.println(configuration(MyConfig.class)));
