42c15d91984b84e9fcd76834e6125399e0a68810
antonbashir <antonbashir@mail.ru>
2021-01-16 22:10:41 +0300

    ART Generator

--- language.kotlin/build.gradle.kts
+++ language.kotlin/build.gradle.kts
@@ -4,6 +4,20 @@ plugins {
 
 dependencies {
     implementation(project(":language.java"))
-    implementation("org.jetbrains.kotlin","kotlin-compiler-embeddable", "1.+")
-    implementation("org.jetbrains.kotlin","kotlin-annotation-processing-gradle", "1.+")
+    implementation("org.jetbrains.kotlin", "kotlin-compiler-embeddable", "1.+")
+    implementation("org.jetbrains.kotlin", "kotlin-annotation-processing-gradle", "1.+")
+}
+
+tasks.withType<JavaCompile> {
+    if (!JavaVersion.current().isJava8) {
+        options.compilerArgs.addAll(arrayOf(
+                "--add-exports", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+                "--add-exports", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+                "--add-exports", "jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED",
+                "--add-exports", "jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+                "--add-exports", "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+                "--add-exports", "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+                "--add-exports", "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED"
+        ))
+    }
 }
