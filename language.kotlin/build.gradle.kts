17d342a00c5497188a9f6e2b5669d9f876d91254
antonbashir <antonbashir@mail.ru>
2021-01-16 22:11:32 +0300

    ART Generator

--- language.kotlin/build.gradle.kts
+++ language.kotlin/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 dependencies {
     implementation(project(":language.java"))
     implementation("org.jetbrains.kotlin", "kotlin-compiler-embeddable", "1.+")
-    implementation("org.jetbrains.kotlin", "kotlin-annotation-processing-gradle", "1.+")
 }
 
 tasks.withType<JavaCompile> {
