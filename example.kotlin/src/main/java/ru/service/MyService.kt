388807c4bdd058dda3fe43e9f540ef8afc34a86f
antonbashir <antonbashir@mail.ru>
2021-02-09 06:14:39 +0300

    ART

--- example.kotlin/src/main/java/ru/service/MyService.kt
+++ example.kotlin/src/main/java/ru/service/MyService.kt
@@ -1,7 +1,7 @@
 package ru.service
 
-import io.art.kotlin.communicator
-import io.art.kotlin.logger
+import io.art.extensions.communicator
+import io.art.extensions.logger
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import ru.communicator.MyClient
