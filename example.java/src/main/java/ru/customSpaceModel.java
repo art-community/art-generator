53fff28238bddae0953b4daf1f2d22b1593b7773
antomy-gc <antomy-gc@gmail.com>
2021-02-09 19:48:54 +0300

    .

--- example.java/src/main/java/ru/customSpaceModel.java
+++
@@ -1,9 +0,0 @@
-package ru;
-
-import io.art.storage.space.*;
-import io.art.value.immutable.*;
-import ru.model.*;
-
-public interface customSpaceModel extends Space<Value, Model> {
-    void nothing();
-}
