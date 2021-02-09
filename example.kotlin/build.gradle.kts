db31f3b58fee9956f008d218f3bf7b410938b1c7
antonbashir <antonbashir@mail.ru>
2021-02-09 06:46:51 +0300

    ART

--- example.kotlin/build.gradle.kts
+++ example.kotlin/build.gradle.kts
@@ -44,7 +44,7 @@ kapt {
             arg("art.generator.recompilation.destination", compileKotlin
                     .destinationDir
                     .absolutePath)
-            arg("art.generator.recompilation.classpath", configurations.compileClasspath.get().files
+            arg("art.generator.recompilation.classpath", compileKotlin.classpath.files
                     .toSet()
                     .filter { it.exists() }
                     .joinToString(","))
