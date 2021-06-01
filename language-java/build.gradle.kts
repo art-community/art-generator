7324ec75646b9e7ade707f4c7b0333f3ecbb7b62
anton.bashirov <anton.sh.local@gmail.com>
2021-06-01 15:44:54 +0300

    ART

--- language-java/build.gradle.kts
+++ language-java/build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation("io.art.java:message-pack:main")
     implementation("io.art.java:scheduler:main")
     implementation("io.art.java:logging:main")
-    implementation("io.art.java:rocks-db:main")
     implementation("io.art.java:storage:main")
     implementation("io.art.java:tarantool:main")
 
