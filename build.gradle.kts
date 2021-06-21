4e4206e3d65a88543fea3cba355865d51d433881
anton.bashirov <anton.sh.local@gmail.com>
2021-06-21 09:52:42 +0300

    ART

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,7 +94,7 @@ executable {
 }
 
 tasks["build"].dependsOn("build-executable-jar")
-tasks["publish"].dependsOn("build-executable-jar")
+tasks.findByName("publish")?.dependsOn("build-executable-jar")
 
 tasks.test {
     dependsOn(WRITE_CONFIGURATION_TASK)
