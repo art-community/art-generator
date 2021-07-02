e383311a108f271506c58b8a9b6f93d19854f32b
anton.bashirov <anton.sh.local@gmail.com>
2021-07-02 04:27:19 +0300

    ART

--- src/main/kotlin/io/art/generator/service/ControllerService.kt
+++ src/main/kotlin/io/art/generator/service/ControllerService.kt
@@ -32,14 +32,56 @@ import kotlin.io.path.exists
 import kotlin.io.path.readText
 import kotlin.io.path.writeText
 
-data class ControllerState(
+private data class ControllerState(
         val state: GeneratorState,
-        val timestamp: LocalDateTime,
-        val count: Int,
+        val timestamp: LocalDateTime? = null,
+        val count: Int = 0,
 )
 
 object ControllerService {
-    fun writeState(state: ControllerState) = configuration.controller.writeText("${state.state}$SHARP${state.timestamp.format(DEFAULT_FORMATTER)}${SHARP}${state.count}")
+    fun controllerFileExists() = configuration.controller.exists()
 
-    fun readState() = configuration.controller.readText()
+    fun isAvailable() = readState().state == AVAILABLE
+
+    fun isStopping() = readState().state == STOPPING
+
+    fun isLocked() = readState().state == LOCKED
+
+    fun stoppingWaiters(): Int {
+        if (!isStopping()) return 0
+        return readState().count
+    }
+
+    fun lockIsValid(): Boolean {
+        if (isAvailable()) return true
+        return readState().timestamp!!.isBefore(now().minus(LOCK_VALIDATION_DURATION))
+    }
+
+    fun updateLock() = writeState(ControllerState(LOCKED, now()))
+
+    fun incrementStopWaiters() = writeState(ControllerState(STOPPING, now(), 1))
+
+    fun markAvailable() = writeState(ControllerState(AVAILABLE))
+
+    private fun writeState(state: ControllerState) {
+        if (!controllerFileExists()) touchDirectory(configuration.controller.parent)
+        if (state.state == AVAILABLE) {
+            configuration.controller.writeText(AVAILABLE.name)
+            return
+        }
+        state.timestamp?.let { stamp ->
+            configuration.controller.writeText("${state.state}$SHARP${stamp.format(DEFAULT_FORMATTER)}${SHARP}${state.count}")
+        }
+    }
+
+    private fun readState(): ControllerState {
+        if (!controllerFileExists()) return ControllerState(state = AVAILABLE)
+        val value = configuration.controller.readText().split(SHARP)
+        if (value.isEmpty()) return ControllerState(state = AVAILABLE)
+        val state = GeneratorState.valueOf(value[0])
+        if (state == AVAILABLE) return ControllerState(state = AVAILABLE)
+        val timestamp = parse(value[1], DEFAULT_FORMATTER)
+        if (value.size == 2) return ControllerState(state = state, timestamp = timestamp)
+        return ControllerState(state = state, timestamp = timestamp, count = value[2].toInt())
+    }
 }
