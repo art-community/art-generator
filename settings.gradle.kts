859883b1da673a3fc7bf1dddb6702dfd031f00a2
anton.bashirov <anton.sh.local@gmail.com>
2021-08-15 05:43:28 +0300

    ART

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.contains("art")) {
-                useModule("io.art.gradle:art-gradle:main")
+                useModule("io.art.gradle:art-gradle:${requested.version}")
             }
         }
     }
