ee0ec6d6b887e712d646cb3494afcba4f8d5c5f3
anton.bashirov <anton.sh.local@gmail.com>
2022-01-01 21:12:09 +0300

    ART

--- src/test/kotlin/io/art/generator/test/GeneratorRunner.kt
+++ src/test/kotlin/io/art/generator/test/GeneratorRunner.kt
@@ -1,7 +1,7 @@
 package io.art.generator.test/*
  * ART
  *
- * Copyright 2019-2021 ART
+ * Copyright 2019-2022 ART
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
